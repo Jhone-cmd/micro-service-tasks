6bb22ab209eabaaa5633e7b28d977f8da86496e5
Jhone-cmd <jhone.1941@gmail.com>
2026-08-17 14:22:17 -0300

    inital commit

---
+++ src/main/java/com/jhonecmd/service_one/controller/ServiceOneController.java
@@ -0,0 +1,18 @@
+package com.jhonecmd.service_one.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceOneController {
+
+    @Value("${message:Hello default")
+    private String message;
+
+
+    @GetMapping("/message")
+    public String message(){
+        return message;
+    }
+}
