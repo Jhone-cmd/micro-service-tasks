0e4e9d626017ff4f509f604e2cd9612eb91be237
Jhone-cmd <jhone.1941@gmail.com>
2026-08-19 15:37:07 -0300

    add tasks controller

--- src/main/java/com/jhonecmd/service_tasks/controller/ServiceTasksController.java
+++ src/main/java/com/jhonecmd/service_tasks/controller/ServiceTasksController.java
@@ -1,11 +1,21 @@
 package com.jhonecmd.service_tasks.controller;
 
+import com.jhonecmd.service_tasks.dto.TasksDTO;
+import com.jhonecmd.service_tasks.model.TasksEntity;
+import com.jhonecmd.service_tasks.service.TasksService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/tasks")
+@RequiredArgsConstructor
 public class ServiceTasksController {
+
+    private final TasksService tasksService;
 
     @Value("${message:Hello default}")
     private String message;
@@ -14,5 +24,18 @@ public class ServiceTasksController {
     @GetMapping("/message")
     public String message(){
         return message;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> save(@RequestBody TasksDTO tasksDTO) {
+        try {
+            TasksEntity tasks = TasksEntity.builder()
+                    .title(tasksDTO.getTitle()).email(tasksDTO.getEmail())
+                    .description(tasksDTO.getDescription()).dueDate(tasksDTO.getDueDate()).build();
+            tasksService.save(tasks);
+            return ResponseEntity.status(HttpStatus.CREATED).body(tasks.getId());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 }
--- src/main/java/com/jhonecmd/service_tasks/model/TasksEntity.java
+++ src/main/java/com/jhonecmd/service_tasks/model/TasksEntity.java
@@ -1,13 +1,19 @@
 package com.jhonecmd.service_tasks.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity(name = "tasks")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TasksEntity {
 
     @Id
