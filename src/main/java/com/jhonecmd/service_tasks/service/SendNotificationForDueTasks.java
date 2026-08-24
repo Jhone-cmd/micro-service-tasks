57b504f1f14dfe7f3f00a43eadea14a9b982573a
Jhone-cmd <jhone.1941@gmail.com>
2026-08-24 15:48:13 -0300

    fine-tuning microservices

--- src/main/java/com/jhonecmd/service_tasks/service/SendNotificationForDueTasks.java
+++ src/main/java/com/jhonecmd/service_tasks/service/SendNotificationForDueTasks.java
@@ -26,6 +26,7 @@ public class SendNotificationForDueTasks {
                     task.getEmail());
             notificationClient.sendNotification(request);
             task.setNotified(true);
+            tasksRepository.save(task);
         }
     }
 }
